142fd3aa94b7e982edb608fc94719409a4ad5624
Lali <lauradiaz1586@gmail.com>
2021-02-21 10:21:54 +0400

    worked on Hero and Navbar components

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import phoneImg from '../assets/phone.svg';
+import { useGlobalContext } from '../context';
 
 const Hero = () => {
-  return <h2>hero component</h2>;
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
+            repellendus dignissimos aut recusandae quo, dolores dolorem esse
+            praesentium, laudantium dolore voluptatum? Quaerat dolorem ex odio
+            unde recusandae pariatur esse doloremque.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
